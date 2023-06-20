d27f3a3b6a92252974dd956e6ada66a2c73a66eb
Micah Elm <micah.b.elm@gmail.com>
2023-06-20 12:47:54 -0700

    move tasks from component state to - app - state so other components have access to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 
 function App() {
+  const [tasks, setTasks] =useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
 
   return (
     <>
     <Header />
     <h1>Saying Hello from This file</h1>
-    <Tasks />
+    <Tasks tasks={tasks} />
     </>
   )
 }
